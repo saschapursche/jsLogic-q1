56ec0b8cff0bca256d258f59614d2e8a8f828941
Sascha Pursche <saschapursche@horstrdslexikon.speedport.ip>
2023-05-14 13:43:50 +0200

    cleaned .js files

--- jsLogic-q1-bonus.js
+++ jsLogic-q1-bonus.js
@@ -5,18 +5,14 @@ const newArray = [];
 
 const compareString = () => {
     let string1 = array[1];
-    console.log(string1);
     let string2 = array[2];
-    console.log(string2);
     arrayStrings.push(string1, string2);
-    console.log(arrayStrings);
 }
 
 const mergingNumbers = () => {
 let int1 = array[0];
 let int2 = array[3];
 arrayInt.push(int1, int2);
-console.log(arrayInt);
 }
 
 compareString();
--- jsLogic-q1.js
+++ jsLogic-q1.js
@@ -2,34 +2,21 @@ const array = [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20];
 
 const sortingArray = () => {
     array.sort((a, b) => a-b);
-    console.log(array);
 };
 
 const mergingNumbers = () => {
-let merging1 = array.filter(num => num ===1)
-    console.log(merging1);
-let merging2 = array.filter(num => num ===2)
-    console.log(merging2);
-let merging20 = array.filter(num => num ===20)
-    console.log(merging20);
+    let merging1 = array.filter(num => num ===1)
+    let merging2 = array.filter(num => num ===2)
+    let merging20 = array.filter(num => num ===20)
 };
 
 const insertingNumbers = () => {
     array.splice(0, 4, merging1);
-    console.log(array);
     array.splice(1, 3, merging2);
-    console.log(array);
     array.splice(5, 2, merging20);
-    console.log(array);
 };
 
 sortingArray();
 mergingNumbers();
 insertingNumbers();
-
-//Clean the room function:
-//given an input of [1,2,4,591,392,391,2,5,10,2,1,1,1,20,20],
-//make a function that organizes these into individual array that is ordered.
-//For example answer(ArrayFromAbove) should return: [[1,1,1,1],[2,2,2], 4,5,10,[20,20], 391, 392,591]. 
-//Bonus: Make it so it organizes strings differently from number types.
-//i.e. [1, "2", "3", 2] should return [[1,2], ["2", "3"]]
+console.log(array);
